0488e86b0f59218fa3270714cbdb4629e10b6d9b
pmatano <pmatano@squaretrade.com>
2017-12-11 13:19:33 -0700

    Remove and update endpoints

--- src/main/java/com/example/purchase/repository/ReceiptPurchaseRepository.java
+++ src/main/java/com/example/purchase/repository/ReceiptPurchaseRepository.java
@@ -3,7 +3,9 @@ package com.example.purchase.repository;
 import com.example.purchase.model.ReceiptPurchase;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface ReceiptPurchaseRepository extends ReactiveMongoRepository<ReceiptPurchase, String> {
+    Flux<ReceiptPurchase> findByUserId(String userId);
 }
